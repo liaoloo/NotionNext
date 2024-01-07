cf241d180558bfe857018ca3a1007402e15689b2
LiaoGuanyi <122196827+liaoloo@users.noreply.github.com>
2024-01-07 09:05:32 +0800

    Update 1.7

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,12 +10,12 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'cn.12.cn.eu.org',
+  HEO_HERO_TITLE_1: '一起编程',
+  HEO_HERO_TITLE_2: '享受编程的乐趣',
+  HEO_HERO_TITLE_3: 'cn.bcltx.link',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0',
-  HEO_HERO_TITLE_LINK: 'cn.12.cn.eu.org',
+  HEO_HERO_TITLE_LINK: 'cn.bcltx.link',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -29,9 +29,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是廖同学',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '你好！我是',
   ],
   HEO_INFO_CARD_URL: 'https://github.com/liaoloo', // 个人资料底部按钮链接
 
@@ -99,7 +97,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: '',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
