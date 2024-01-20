5f30fc1716bb62f6abaee7776380bc501eabc4ea
LiaoGuanyi <122196827+liaoloo@users.noreply.github.com>
2024-01-20 13:07:14 +0800

    Update 0120

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,9 +18,10 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: '12.cn.eu.org',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '首页', url: '/' },
+  HEO_HERO_CATEGORY_2: { title: '项目', url: '/tag/项目'},
+  HEO_HERO_CATEGORY_3: { title: '分享', url: '/tag/分享' },
+ 
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
